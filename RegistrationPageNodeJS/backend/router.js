9ab0e39431f32642e93f1860b215607cff68aa41
Ankur <Ankur.gahtori@daffodilsw.com>
2020-02-18 14:18:18 +0530

    Final commit

--- RegistrationPageNodeJS/backend/router.js
+++ RegistrationPageNodeJS/backend/router.js
@@ -3,25 +3,27 @@ const router = express.Router();
 const api = require('./api')
 const fs = require('fs')
 router.get("/", (req, res) => {
-    res.send("<script>location.href='current/index2.html'</script>");
+    console.log("here cookies are: ",req.cookies);
+    res.render('index9.html');
 })
-
 router.post("/createUser", async (req, res) => {
     try {
         await api.getUserById(req.body.email);
         await api.createUser(req.body);
-        res.end('<script>location.href="current/index2.html"</script>');
+        res.end('<script>location.href="index2.html"</script>');
     } catch (err) {
-        res.send('<script>alert("Email address already exist");location.href="current/index.html"</script>');
+        res.send('<script>alert("Email address already exist");location.href="index9.html"</script>');
     }
 })
 
 router.get('/allUser', async (req, res) => {
     try {
         let data = await api.allUser();
-        res.send(data);
+         res.render('admin/adminpage.html',{'data':data});
+       // res.send(data);
     } catch (err) {
-        end("some error might be there");
+        console.log("error");
+        res.end("some error might be there");
     }
 })
 router.post("/loginUser", async (req, res) => {
@@ -30,7 +32,8 @@ router.post("/loginUser", async (req, res) => {
         res.redirect("actualgame/index3.html");
 
     } catch (err) {
-        res.send("<script>alert(\"Invalid detail\");location.href='index2.html'</script>");
+        res.write("<script>alert('Invalid detail');location.href='index2.html'</script>");
+        res.end();
     }
 })
 module.exports = router;
--- RegistrationPageNodeJS/backend/server.js
+++ RegistrationPageNodeJS/backend/server.js
@@ -1,19 +1,29 @@
 var express=require('express');
-var app=express();
-const port=8081;
-const hotname="192.168.100.189";
+var cookieParser=require('cookie-parser');
+var ejs=require('ejs');
 const router=require("./router");
 const bodyParser=require('body-parser')
+const port=8081;
+const hostname="0.0.0.0";
+
+
+var app=express();
+
+
 app.use(bodyParser.urlencoded({ extended: false }))
+app.engine('html',ejs.renderFile)
+app.set('view engine','html')
+app.set('views','RegistrationPageNodeJS/frontend')
+app.use(cookieParser());
 app.use(express.static('RegistrationPageNodeJS/frontend'))
 const mongoose=require('mongoose');
 //ejs
 mongoose.connect("mongodb://localhost:27017/detail",(err,result)=>{
-    if(result)console.log("connected successfully");
+    if(result)console.log("connected to Database successfully");
     else console.log("Error");
 })
 
-app.listen(port,hotname,()=>{
-    console.log("Server running");
+app.listen(port,hostname,()=>{
+    console.log(`Server running -> ${hostname}:${port}`);
 })
 app.use("/",router);
